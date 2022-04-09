49586c54186c34d9f7ce76f46781e7c5a954cfd4
Nathan Willer <nathanielwiller93@gmail.com>
2022-04-08 20:44:51 -0400

    Select and checkout branch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use cursive::Cursive;
-use cursive::views::Dialog;
-use git2::{Repository, BranchType, Branch, Error};
+//#![allow(unused_imports)]
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode},
+    terminal,
+    terminal::ClearType,
+    ExecutableCommand,
+};
+use git2::{Branch, BranchType, Error, Repository};
+use std::io;
+use std::io::stdout;
 
+struct Cleanup;
 
-fn main() {
+impl Drop for Cleanup {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Failed to disable Raw Mode.");
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    let _clean_up = Cleanup;
+
     let repo = match Repository::open("/Users/reina/school/groupwork/capstone/") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
@@ -11,42 +28,66 @@ fn main() {
 
     let branches = get_local_branches(&repo);
     let branch_names = get_branch_name(&branches).unwrap();
-    //checkout_branch(&repo, branch_names.get(1).unwrap());
 
-    let mut siv = cursive::default();
+    terminal::enable_raw_mode()?;
+    let mut stdout = stdout();
+    stdout
+        .execute(terminal::Clear(ClearType::All))?
+        .execute(cursor::MoveTo(0, 0))?;
+    print_branches(&branch_names);
+    if let Some(pos) = print_events(&stdout)? {
+        let branch = branch_names.get(pos as usize).unwrap();
+        checkout_branch(&repo, branch);
+        stdout
+            .execute(terminal::Clear(ClearType::All))?
+            .execute(cursor::MoveTo(0, 0))?;
+        println!("Switched to branch: {branch}\r");
+    };
+    terminal::disable_raw_mode().expect("Failed to disable Raw Mode.");
 
-    siv.add_global_callback('q', |s| s.quit());
-
-    siv.add_layer(Dialog::text(branch_names.join("\n"))
-        .title("Important")
-        .button("Next", show_next));
-
-    siv.run();
+    Ok(())
 }
 
-#[allow(dead_code)]
-fn show_next(s: &mut Cursive) {
-    s.pop_layer();
-    s.add_layer(Dialog::text("Did you do the thing?")
-        .title("Question 1")
-        .button("Yes!", |s| show_answer(s, "Well done!"))
-        .button("No", |s| show_answer(s, "I knew you couldn't be trusted!!!"))
-        .button("Uh...?", |s| s.add_layer(Dialog::info("Try again!"))));
+fn print_branches(branches: &[&str]) {
+    for branch in branches {
+        println!("{branch}\r");
+    }
 }
 
-#[allow(dead_code)]
-fn show_answer(s: &mut Cursive, msg: &str) {
-    s.pop_layer();
-    s.add_layer(Dialog::text(msg)
-        .title("Results")
-        .button("Finish", |s| s.quit()));
+fn print_events(mut stdout: &std::io::Stdout) -> crossterm::Result<Option<u16>> {
+    loop {
+        match read()? {
+            Event::Key(event) => {
+                match event.code {
+                    KeyCode::Char('q') => {
+                        break;
+                    }
+                    KeyCode::Char('j') => {
+                        stdout.execute(cursor::MoveDown(1))?;
+                    }
+                    KeyCode::Char('k') => {
+                        stdout.execute(cursor::MoveUp(1))?;
+                    }
+                    KeyCode::Enter => {
+                        let pos = cursor::position().unwrap();
+                        return Ok(Some(pos.1));
+                    }
+                    _ => {}
+                };
+            }
+            Event::Mouse(event) => println!("{:?}", event),
+            Event::Resize(width, height) => println!("New Size {}x{}", width, height),
+        }
+    }
+    Ok(None)
 }
 
 fn get_local_branches(repo: &Repository) -> Vec<Branch> {
     let mut local_branches: Vec<Branch> = Vec::new();
 
     let branches = repo.branches(Some(BranchType::Local)).unwrap();
-    let branches: Vec<Result<(Branch, BranchType), Error>> = branches.collect::<Vec<Result<_, _>>>();
+    let branches: Vec<Result<(Branch, BranchType), Error>> =
+        branches.collect::<Vec<Result<_, _>>>();
 
     for branch in branches {
         match branch {
@@ -77,13 +118,16 @@ fn get_branch_name<'a>(branches: &'a [Branch]) -> Result<Vec<&'a str>, Error> {
 }
 
 fn checkout_branch(repo: &Repository, refname: &str) {
+    // Need to change the files in the working directory as well as set the HEAD
     let (object, reference) = repo.revparse_ext(refname).expect("Object not found");
-    repo.checkout_tree(&object, None).expect("Failed to checkout");
+    repo.checkout_tree(&object, None)
+        .expect("Failed to checkout");
 
     match reference {
         // gref is an actual reference like branches or tags
         Some(gref) => repo.set_head(gref.name().unwrap()),
         // this is a commit, not a reference
         None => repo.set_head_detached(object.id()),
-    }.expect("Failed to set HEAD");
+    }
+    .expect("Failed to set HEAD");
 }
