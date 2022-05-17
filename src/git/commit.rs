c9531768f1e98e35b96063b4b31d24593f3c3ee4
Nathan Willer <nathanielwiller93@gmail.com>
2022-05-17 14:59:06 -0400

    Create signature and commit

---
+++ src/git/commit.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use git2::{Config, Signature};
+use std::path::PathBuf;
+
+use crate::git::repo;
+
+pub fn commit(repo_path: PathBuf, message: &str) -> Result<()> {
+    let repo = repo(&repo_path)?;
+    let signature = signature()?;
+
+    let mut index = repo.index()?;
+    let id = index.write_tree()?;
+    let tree = repo.find_tree(id)?;
+
+    if let Some(head) = repo.head()?.target() {
+        let commit = vec![repo.find_commit(head)?];
+        let parents = commit.iter().collect::<Vec<_>>();
+        repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            message,
+            &tree,
+            parents.as_slice(),
+        )?;
+    }
+    Ok(())
+}
+
+fn signature() -> Result<Signature<'static>> {
+    let config = Config::open_default()?;
+    if let Some(name) = config.get_entry("user.name")?.value() {
+        if let Some(email) = config.get_entry("user.email")?.value() {
+            let signature = Signature::now(name, email)?;
+            return Ok(signature);
+        }
+        let signature = Signature::now(name, "")?;
+        return Ok(signature);
+    }
+
+    let signature = Signature::now("", "")?;
+    Ok(signature)
+}
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -7,6 +7,7 @@ pub mod git_diff;
 pub mod git_log;
 pub mod git_status;
 pub mod stage;
+pub mod commit;
 
 pub fn repo(repo_path: &Path) -> Result<Repository> {
     let repo = Repository::init(repo_path)?;
